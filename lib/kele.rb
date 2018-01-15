a0d11fa1f8c9fe86bc30ec71a48366c34d5476e5
Chris Gallagher <cgally103@gmail.com>
2018-01-15 17:50:57 -0500

    retrieve user date and convert into a ruby hash

--- lib/kele.rb
+++ lib/kele.rb
@@ -11,9 +11,18 @@ class Kele
     p "Error invalid credentials" if @auth_token.nil?
 	end
 
+	def get_me
+		response = self.class.get(user_url, headers: { "authorization" => @auth_token })
+		JSON.parse(response.body)
+	end
+
 	private
 
 	def base_url
 		"https://www.bloc.io/api/v1/sessions"
+	end
+
+	def user_url
+		"https://www.bloc.io/api/v1/users/me"
 	end
 end
